673c3ecdad5911d5a0b4b62be785391ac0cffd9d
Pavel <Pavel.Pasechnik@gmail.com>
2024-02-15 15:57:25 +0200

    added color for reset button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -6,7 +6,13 @@ export default function Options({ updateFeedback, totalFeedback, resetFeedback }
       <button onClick={() => updateFeedback('good')}>Good</button>
       <button onClick={() => updateFeedback('neutral')}>Neutral</button>
       <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {totalFeedback <= 0 ? '' : <button onClick={resetFeedback}>Reset</button>}
+      {totalFeedback <= 0 ? (
+        ''
+      ) : (
+        <button onClick={resetFeedback} className={css.reset}>
+          Reset
+        </button>
+      )}
     </div>
   );
 }
